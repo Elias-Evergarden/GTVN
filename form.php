92108415d29b26344b025a3d082683a1f490a383
H <claryvalentinemorgenstern@gmail.com>
2020-07-26 10:42:51 +0700

    initial commit

---
+++ form.php
@@ -0,0 +1,11 @@
+<?php
+
+// Get data from forms
+
+?>
+
+<form action="dashboard.php" method="post">
+	<input type="text" name="userid">
+	<input type="password" name="password">
+	<input type="submit" value="Log In">
+</form>
